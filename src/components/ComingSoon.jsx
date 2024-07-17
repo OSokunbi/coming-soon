bab6b30dace57bedf73937569d58d52997bd83f2
Oreoluwa Sokunbi <osokunbi@umich.edu>
2024-07-16 20:15:40 -0400

    coming soon

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo-base-256x256.png';
 
 const ComingSoon = () => {
   return (
-    <div className="flex items-center justify-center w-full h-screen">
+    <div className="flex items-center justify-center w-full">
       <div className="text-center">
         <img src={logo} alt="logo" className="w-20 h-20 mx-auto mb-4 animate-spin" />
       </div>
